f92a9f53a9875be54610b7de05c4637326ad7398
kon <k@smo.la>
2023-07-28 23:04:13 -0700

    try some python

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM ubuntu:22.04
 RUN apt-get update \
-    && apt-get install -y python3 usbutils
+    && apt-get install -y python3 python3-pip usbutils
+RUN pip install pyserial
 COPY src/ /dst/
--- src/run_tests.py
+++ src/run_tests.py
@@ -1,3 +1,14 @@
 import os
+import sys
+import serial
 
 print("success")
+
+def runTests():
+    return
+
+def main():
+    port = sys.argv[1]
+    ser = serial.Serial(port, 115200, timeout=1)
+    numTestFailures = runTests(ser)
+    sys.exit(numTestFailures)
